47612dcbd528ae989f55c717c553c882e842289e
Ankhit <aapandurangi@ucsd.edu>
2019-06-15 19:00:26 +0100

    new landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_scope :user do
     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get "/" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
   end
   devise_for :users
   get 'corner/The'
   get 'corner/Corner'
   get 'login_page/login'
-  root :to => 'devise/registrations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
