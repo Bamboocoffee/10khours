6b0efc595b2692d9af1b7cd192acae3f55fcae42
Ankhit <aapandurangi@ucsd.edu>
2019-06-15 18:59:20 +0100

    new landing page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get 'corner/The'
   get 'corner/Corner'
   get 'login_page/login'
-  root :to => 'login_page#login'
+  root :to => 'devise/registrations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
