e339f7b9caedd7f408689bf1247b8a61d95496ea
Ankhit <aapandurangi@ucsd.edu>
2019-06-17 20:54:31 +0100

    I might fuck everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'landing_page#index'
   devise_scope :user do
-    get "/here" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get "/sign_in" => "devise/sessions#new", as: "new_user_sessions" # custom path to sign_up/registration
+    get "/dashboard" => "dashboard#mydash", as: "user_dashboard"
+
   end
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   resources :users do
     resources :landing_page
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_180938) do
+ActiveRecord::Schema.define(version: 2019_06_17_175036) do
+
+  create_table "dashboards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "landing_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
